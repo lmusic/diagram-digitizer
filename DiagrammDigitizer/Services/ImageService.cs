ce0112b13c55ae8821d2dcfd1ad21fa247200616
lmusic <denisko_dmitriy@mail.ru>
2020-02-27 12:56:57 +0700

    Implement digitize for horizontal diagrams

--- DiagrammDigitizer/Services/ImageService.cs
+++ DiagrammDigitizer/Services/ImageService.cs
@@ -29,17 +29,17 @@ namespace DiagramDigitizer.Services
             int maxGreenX = 0;
 
             var listOfBluePoints = new List<Point>();
-            var listOfRedPoints = new List<Point>();
+            var listOfGreenPoints = new List<Point>();
             var result = new double[360];
 
-            for (var i = 0; i < image.Width; i++)
+            for (var i = 0; i < image.Height; i++)
             {
-                for (var j = 0; j < image.Height; j++)
+                for (var j = 0; j < image.Width; j++)
                 {
-                    var pixel = image.GetPixel(i, j);
+                    var pixel = image.GetPixel(j, i);
                     if (pixel.B > 200 && pixel.G < 200 && pixel.R < 200)
                     {
-                        listOfBluePoints.Add(new Point(i,j));
+                        listOfBluePoints.Add(new Point(j,i));
 
                         if (maxBlueX < j)
                         {
@@ -52,34 +52,89 @@ namespace DiagramDigitizer.Services
                         {
                             maxGreenX = j;
                         }
-                    }
-                    if (pixel.B < 200 && pixel.G < 200 && pixel.R > 200)
-                    {
-                        listOfRedPoints.Add(new Point(i, j));
+
+                        listOfGreenPoints.Add(new Point(j, i));
                     }
                 }
             }
 
             var stepOfPixel = Convert.ToDouble(3) / Convert.ToDouble(maxBlueX - maxGreenX);
-            var center = FindCenter(listOfRedPoints);
+            var center = FindCenter(listOfGreenPoints);
             var lengthOfLineFromCenterToRound = maxBlueX - center.X;
-            var valueInCenter = lengthOfLineFromCenterToRound * stepOfPixel;
+            var listOfValuesForAngle = new SortedList<int, List<double>>();
+            image.SetPixel(center.X, center.Y, Color.Aqua);
+            SaveImage(image);
 
             listOfBluePoints.ForEach(point =>
             {
                 var angle = GetAngleBetweenPoints(center, point);
-                var valueInPoint = CalculateValueInPoint(stepOfPixel, valueInCenter, center, point);
-                result[Convert.ToInt32(angle)] = valueInPoint;
-                
+                if (angle> 28 && angle<32)
+                {
+                    var g = 2;
+                }
+                var valueInPoint = CalculateValueInPoint(lengthOfLineFromCenterToRound, stepOfPixel, center, point);
+                var i = Convert.ToInt32(angle);
+                if (listOfValuesForAngle.ContainsKey(i))
+                {
+                    listOfValuesForAngle[i].Add(valueInPoint);
+                }
+                else
+                {
+                    listOfValuesForAngle.Add(i, new List<double>{valueInPoint});
+                }
             });
+
+            foreach (var item in listOfValuesForAngle)
+            {
+                double average = 0;
+                item.Value.ForEach(value => { average = average + value; });
+                average = average / item.Value.Count;
+                result[item.Key == 360 ? 0 : item.Key] = average;
+            }
+
+            for (var i = 1; i < result.Length; i++)
+            {
+                if (result[i] <= 0)
+                {
+                    result[i] = InterpolateValue(i, result);
+                }
+            }
             return result;
 
         }
 
-        private double CalculateValueInPoint(double stepOfValueForEachPixel, double valueInCenter, Point center, Point point)
+      private double InterpolateValue(int index, double[] array)
+      {
+          double prevValue = 0;
+          double nextValue = 0;
+          double nextValueIndex = 0;
+          double prevValueIndex = 0;
+          for (int i = index + 1; i < array.Length; i++)
+          {
+              if (array[i] <= 0) continue;
+              nextValue = array[i];
+              nextValueIndex = i;
+              break;
+          }
+
+          for (int i = index - 1; i > 0; i--)
+          {
+              if (array[i] <= 0) continue;
+              prevValue = array[i];
+              prevValueIndex = i;
+              break;
+          }
+          
+          var result = (((nextValue - prevValue) / (nextValueIndex + prevValueIndex))*(index-prevValueIndex));
+
+          return Math.Abs(prevValue < nextValue ? (prevValue+result) : (prevValue-result));
+
+      }
+
+        private double CalculateValueInPoint(double distanceFromCenterToRound, double stepOfValueForEachPixel, Point center, Point point)
         {
             var distanceFromCenterToPoint = GetDistanceBetweenPoints(center, point);
-            var valueInPoint = valueInCenter - (distanceFromCenterToPoint * stepOfValueForEachPixel);
+            var valueInPoint = (distanceFromCenterToRound - distanceFromCenterToPoint) * stepOfValueForEachPixel;
 
             return valueInPoint;
         }
@@ -87,13 +142,13 @@ namespace DiagramDigitizer.Services
 
         private double GetAngleBetweenPoints(Point p1, Point p2)
         {
-            var angle = Math.Atan2(p1.Y - p2.Y, p1.X - p2.X) / Math.PI * 180;
+            var angle = 180 - (Math.Atan2(p1.Y - p2.Y, p1.X - p2.X) / Math.PI * 180);
             return (angle < 0) ? angle + 360 : angle;
         }
 
         private double GetDistanceBetweenPoints(Point p1, Point p2)
         {
-            return Math.Sqrt((p2.X - p1.X) ^ 2 + (p2.Y - p1.Y) ^ 2);
+            return Math.Sqrt(((p2.X - p1.X)*(p2.X - p1.X)) + ((p2.Y - p1.Y)*(p2.Y - p1.Y)));
         }
 
         private Bitmap ConvertGreenColor(Bitmap image)
@@ -187,7 +242,7 @@ namespace DiagramDigitizer.Services
 
             for (var i = 0; i < horizontal.Length; i++)
             {
-                horizontal[i] = Math.Sqrt(i);
+                //horizontal[i] = Math.Sqrt(i);
                 vertical[i] = Math.Sqrt(i);
             }
             var diagram = new Diagram("TestName", 1800,horizontal, vertical);
